a1585793c44ef7da5812c44621c3e62020089b0b
Hazem4215 <hazemff2020@gmail.com>
2024-09-06 19:25:40 +0300

    change2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
+import yaml from '@rollup/plugin-yaml';
+
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), tailwind()],
+
+  vite: {
+    plugins: [yaml()]
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
